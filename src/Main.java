5d786183a40994e38fe518a2896bf64f7b3dac63
HoangThiThi199 <hoangthithi19906@gmail.com>
2025-10-01 18:08:35 +0700

    Update Main.java
    Demo Main(javaFX)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane; // Canvas or Image
+import javafx.stage.Stage;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,14 +15,30 @@ public class Main {
         // Tạo GamePanel
         GamePanel panel = new GamePanel();
         frame.add(panel);
+public class Main extends Application {
+    @Override // ghi đè lên phg thức start của lớp Application
+    public void start(Stage stage) {
 
         // Hiển thị JFrame
         frame.setVisible(true);
+        GamePanel panel = new GamePanel(480, 820); // width, height
+        StackPane root = new StackPane(panel);//Scene chỉ nhận Parent -> cần tạo root
+        // panel là 1 Node con của root
+        Scene scene = new Scene(root, 480, 820);
 
         // Bắt panel nhận focus để nhận bàn phím
         panel.requestFocusInWindow();
+        stage.setTitle("Brick Breaker Demo"); // tên tiêu đề
+        stage.setScene(scene); // hiển thị UI
+        stage.show(); // show cửa sổ game
 
         // Bắt đầu vòng lặp game
         panel.startGameLoop();
+        // Đảm bảo panel nhận focus để bắt phím
+        panel.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // chạy JavaFX
     }
 }
